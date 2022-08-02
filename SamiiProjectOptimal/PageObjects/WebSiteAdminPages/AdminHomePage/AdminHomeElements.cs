c1a184d2c9f3fd816bca81609671f4667e3eade2
Максим Перевалов <nnpctonsp@gmail.com>
2022-08-02 12:47:08 +0300

    02.08.2022 12 : 40

--- SamiiProjectOptimal/PageObjects/WebSiteAdminPages/AdminHomePage/AdminHomeElements.cs
+++ SamiiProjectOptimal/PageObjects/WebSiteAdminPages/AdminHomePage/AdminHomeElements.cs
@@ -19,14 +19,14 @@ namespace SamiiProjectOptimal.PageObjects.WebSiteAdminPages.AdminHomePage
         public readonly static By _PaginationForTableEducator = By.XPath("//nav[@class = 'pagination ng-star-inserted']");
 
         public IWebElement ButtonThreePagination => Browser._Driver.FindElement(_ButtonThreePagination);
-        public readonly static By _ButtonThreePagination = By.XPath("//nav[@class = 'pagination ng-star-inserted']/li[4]");
+        public readonly static By _ButtonThreePagination = By.XPath("//nav[@class = 'pagination ng-star-inserted']/li[5]");
 
         #endregion
 
         #region TableEducator
 
         public IWebElement ButtonDetailsTestTeachern => Browser._Driver.FindElement(_ButtonDetailsTestTeacher);
-        public readonly static By _ButtonDetailsTestTeacher = By.XPath("//table[@class = 'table table-hover']//tr[10]//td[12]//button");
+        public readonly static By _ButtonDetailsTestTeacher = By.XPath("//table[@class = 'table table-hover']//tr[5]//td[12]//button");
 
         #endregion
     }
